8e48010aede062f6a0cf1ddc8428c8d1f10eb65e
jin-dev22 <jinpark.dev22@gmail.com>
2022-07-11 20:40:48 +0900

    testGit init!

---
+++ src/main/webapp/WEB-INF/classes/testGitCo/controller/TestGitServlet.java
@@ -0,0 +1,24 @@
+package testGitCo.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestGitServlet
+ */
+@WebServlet("/testGit")
+public class TestGitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		 response.getWriter().append("helloworld");
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>여기서 연습합시당</title>
+</head>
+<body>
+<h1>test</h1>
+<a href="<%=request.getContextPath()%>/testGit">testGit</a>
+</body>
+</html>
